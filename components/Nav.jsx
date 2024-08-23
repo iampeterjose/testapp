d6f0e994f44296e8c4b1c6e6fc3733b854e04d95
iampeterjose <peterjose098@gmail.com>
2024-08-23 13:12:28 +0800

    footer changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,8 +23,8 @@ const Nav = () => {
     }, []);
 
     return (
-        <div className="shadow-md w-full fixed top-0 left-0">
-            <div className="md:flex items-center justify-between bg-slate-50 py-4 md:px-10 px-7">
+        <div className="shadow-md w-full fixed -top-10 left-0">
+            <div className="md:flex items-center justify-between bg-slate-50 py-4 md:px-10 px-7 pt-20">
                 <div className="font-bold text-2xl cursor-pointer flex items-center font-satoshi text-gray-800 w-[150px]">
                     <a href="/" className="flex justify-center items-center text-xl font-semibold pr-80">
                         <img src="/assets/icons/coffeecup.png" alt="Logo" width={50} height={50} /> 
@@ -32,7 +32,7 @@ const Nav = () => {
                     </a>
                 </div>
 
-                <div onClick={toggleMenu} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+                <div onClick={toggleMenu} className="text-3xl absolute right-8 top-20 pt-5 cursor-pointer md:hidden">
                     <img src="/assets/icons/hamburger.png" alt="Hamburger" width={25} height={25} />
                 </div>
                 <ul className={`md:flex md:items-center md:pb-0 pb-10 absolute md:static bg-slate-50 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${isMenuOpen ? 'top-20 shadow-lg':'top-[-490px]'} md:shadow-none`}>
